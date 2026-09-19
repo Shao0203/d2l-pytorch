4a5451f0f0c0cb7ab604c657193dbe5d0d629a18
Shao0203 <nzsyl0203@gmail.com>
2026-09-19 11:16:06 +0800

    Complete the Tutorial from pyTorch official site.

--- test.py
+++ test.py
@@ -286,7 +286,7 @@ loss.backward()  # calc gradients and save to w.grad and b.grad
 
 
 '''6. 优化模型参数
-'''
+
 training_data = datasets.FashionMNIST(
     root='data',
     train=True,
@@ -363,3 +363,109 @@ for epoch in range(5):
     train_loop(train_dataloader, model, loss_fn, optimizer)
     test_loop(test_dataloader, model, loss_fn)
 print('Done!')
+
+torch.save(model.state_dict(), 'model_weights.pth')  # save model params
+
+new_model = NeuralNetwork()
+new_model.load_state_dict(torch.load('model_weights.pth', weights_only=True))  # load model params
+'''
+
+
+'''7. 全部流程跑通
+'''
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(784, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+def train(dataloader, model, loss_fn, optimizer, device='mps'):
+    model.train()
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+        pred = model(X)
+        loss = loss_fn(pred, y)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch+1) * len(X)
+            print(f'loss: {loss:>7f} [{current:>5d}/{size:>5d}]')
+
+
+def test(dataloader, model, loss_fn, device='mps'):
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f'Test Error: \n Accuracy: {correct:.1%} | Avg loss: {test_loss:>8f} \n')
+
+
+training_data = datasets.FashionMNIST(
+    root='data',
+    train=True,
+    download=True,
+    transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float, scale=True)]),
+)
+
+test_data = datasets.FashionMNIST(
+    root='data',
+    train=False,
+    download=True,
+    transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float, scale=True)]),
+)
+
+# hyper-params
+lr = 1e-3
+batch_size = 64
+epochs = 5
+
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else 'cpu'
+train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+model = NeuralNetwork().to(device)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr)
+
+for epoch in range(epochs):
+    print(f'Epoch {epoch+1} \n----------------------------')
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+
+torch.save(model.state_dict(), 'model.pth')
+print('Done!')
+
+new_model = NeuralNetwork().to(device)
+new_model.load_state_dict(torch.load('model.pth', weights_only=True))
+new_model.eval()
+
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = new_model(x)
+    print(f'Predicted: {pred.argmax(1).item()}, Actual: {y}')  # Predicted: 9, Actual: 9
